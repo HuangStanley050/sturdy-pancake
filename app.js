8cfca567d441458d1accfa97619e2f6a4489da01
Stanley Huang <huang.stanley050@gmail.com>
2020-01-18 11:24:00 +1100

    hooked up graphlql playground to express-graphql

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ const express = require("express");
 const expressGraphQL = require("express-graphql");
 const expressPlayground = require("graphql-playground-middleware-express")
   .default;
+const schema = require("./schema/schema");
 const app = express();
 
-app.use(
-  "/graphql",
-  expressGraphQL({
-    graphiql: false
-  }),
-  expressPlayground({ endpoint: "/graphql" })
-);
+app.use("/graphql", express.json(), expressGraphQL({ schema }));
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 
 module.exports = app;
--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,7 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const users = [{ id: "23", name: "Bill" }, { id: "22", name: "Alex" }];
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
@@ -8,4 +9,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return users.find(user => user.id === args.id);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
